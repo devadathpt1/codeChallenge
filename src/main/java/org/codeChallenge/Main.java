0244d6e5ee3174a4279b8a341385c449e4830a1e
devadath.tabeti <devadath.tabeti@identitye2e.com>
2023-09-14 20:44:45 +0100

    first commit

---
+++ src/main/java/org/codeChallenge/Main.java
@@ -0,0 +1,38 @@
+package org.codeChallenge;
+
+import java.util.Arrays;
+import java.util.*;
+public class Main {
+
+    static int printNumOfPairs(Integer arr[], int sum)
+    {
+         int count = 0;
+
+        Set arr1 = new TreeSet<Integer>(Arrays.asList(arr));
+        Integer arr2[] =    Arrays.copyOf(arr1.toArray(),arr1.size(),Integer[].class);
+
+        for (int i = 0; i < arr2.length; i++)
+            for (int j = i + 1; j < arr2.length; j++)
+                if (arr2[i] + arr2[j] == sum)
+                count++;
+        return count;
+    }
+
+    // Driver Code
+    public static void main(String[] arg)
+    {
+
+        int sumOfPair=1;
+        Integer arr[] = {3, 4, 5, 6};
+        System.out.println(printNumOfPairs(arr,sumOfPair));
+
+        sumOfPair =  15;
+        Integer arr1[] = {0, 15, 32, 2000, 15000};
+        System.out.println(printNumOfPairs(arr1,sumOfPair));
+
+        sumOfPair =  42;
+        Integer[] arr2 = {1, 1, 10, 32, 41};
+        System.out.println(printNumOfPairs(arr2,sumOfPair));
+    }
+
+    }
